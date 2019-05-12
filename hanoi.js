7a914a4012cc74fd1d1af7d6c6edf2d17c447b92
ddang9390 <dnld1ng@yahoo.com>
2019-05-12 17:14:53 -0400

    Hanoi game plays properly

--- hanoi.js
+++ hanoi.js
@@ -1,15 +1,49 @@
 var blocks = document.getElementsByClassName("block");
+var stack1 = document.getElementById("stack1");
+var stack2 = document.getElementById("stack2");
+var goal = document.getElementById("stack3");
+var gameover = false;
+var draggeddisc;
 
 function allowDrop(ev) {
-    ev.preventDefault();
+    var stack = ev.currentTarget;
+
+    var totalBlocks = stack.getElementsByClassName("block");
+    if(totalBlocks.length == 0 || totalBlocks[0].id > draggeddisc){
+        ev.preventDefault();
+        return;
+    }
+    
 }
 
 function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
+    if(ev.target.parentElement.children[0].id == ev.target.id){
+        ev.dataTransfer.setData("text", ev.target.id);
+        draggeddisc = ev.target.id;
+    }
+    else{
+        ev.preventDefault();
+    }
+    
 }
 
 function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
+
+    var stack = ev.target;
+    var firstChild = stack.children[0];
+
+    stack.insertBefore(document.getElementById(data), firstChild);
+    //stack.appendChild(document.getElementById(data));
+}
+
+function checkGoal(){
+    if(goal.children.length == 3){
+        gameover = true;
+    }
+}
+
+function resetGame(){
+    location.reload();
 }
